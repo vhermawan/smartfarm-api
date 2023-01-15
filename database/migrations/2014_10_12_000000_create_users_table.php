2b3120737f65ac5f7de7b2ad689cd47ba7d7fac4
Vicky Hermawan <107031434+vhermawan@users.noreply.github.com>
2023-01-15 16:31:29 +0700

    feat: add passport

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id_users');
-            $table->string('nama');
+            $table->string('name');
+            $table->string('email');
             $table->string('username')->unique();
             $table->string('password');
-            $table->text('alamat');
+            $table->text('address');
             $table->softDeletes();
             $table->timestamps();
         });
